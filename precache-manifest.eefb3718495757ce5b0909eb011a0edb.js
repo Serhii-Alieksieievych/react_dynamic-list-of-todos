80db69fc5cc18a806a9928ff9c00130b3115a2f8
Serhii Alieksieievych <kajot1863@gmail.com>
2021-01-26 17:14:43 +0200

    Updates

---
+++ precache-manifest.eefb3718495757ce5b0909eb011a0edb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "214f003a1e3fc24b8aed47fc8daa24a4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "73405bd039ba9383f335",
+    "url": "./static/css/main.214c70b5.chunk.css"
+  },
+  {
+    "revision": "8ca640667d29a6b1f45d",
+    "url": "./static/js/2.5502a536.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.5502a536.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73405bd039ba9383f335",
+    "url": "./static/js/main.98a981df.chunk.js"
+  },
+  {
+    "revision": "0c165204287428b65967",
+    "url": "./static/js/runtime-main.a37292e9.js"
+  }
+]);
